23f7cde35d5252ac8fa543e900b1d50d5621101b
mama-d'lo <109547512+mama-dlo@users.noreply.github.com>
2022-09-02 18:54:19 -0700

    utils - file parse into dict of lists

--- helpers/utils.py
+++ helpers/utils.py
@@ -1,9 +1,31 @@
+pfile = "flaskr/static/io/input_sample.txt"
+
+def line_check(line, l):
+    line = line.lower()
+    if l == 0:
+        if line[:8] != 'target: ':
+            return "Target not found."
+    try: 
+        return int(line[8:])
+    except ValueError:
+        return "ValueError. Floating points are not accepted as targets."
 
 
-file = "PyramidDescent/static/io/input_sample.txt"
 def parse(file):
-    samp = []
+    parsed_dict = {}
+    target = 1
     with open(file) as f:
-        for line in f:
-            samp = line.split(",").append()
-    return samp
+        for l, line in enumerate(f):
+            if l == 0: # Does the check once
+                target = line_check(line, l)
+                if isinstance(target, str):
+                    return f" Could not continue: {target}"
+            if l > 0: # Begin dict population from here
+                try:
+                    parsed_dict[l] = [int(i.rstrip()) for i in line.split(',')]
+                except ValueError as e:
+                    return e
+        parsed_dict[0] = int(target)
+    return parsed_dict
+
+print(parse(pfile))
--- router/home.py
+++ router/home.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, Blueprint, request, render_template
 from werkzeug.utils import secure_filename
+from helpers.utils import parse
 
 home = Blueprint('home', __name__)
 app = Flask(__name__)
@@ -14,14 +15,16 @@ def result():
     if request.method == 'POST':
         txt=""
         f = request.files['txt_input']
-
         filename = secure_filename(f.filename)
-
         f.save('flaskr/static/io/' + filename)
-        file = open('flaskr/static/io/' + filename,"r")
-        txt = file.readlines()
+        #opened_file = open('flaskr/static/io/' + filename,"r")
+        # txt = opened_file.readlines()
 
         #TODO: implement puzzle and send thru template
+        txt = parse(filename)
+        if isinstance(txt, ValueError):
+            txt = None
+
     return render_template('result.html', txt=txt)
 
 if __name__ == '__main__':
